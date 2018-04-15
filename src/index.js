d9f2e58bfd698774944de9162b7f0734cde85d1e
Michael1935 <291140738@qq.com>
2018-04-15 21:33:07 +0800

    add react-redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { counter, addGUN, minGUN, addGUNAsync } from './index.redux';
+// import { counter, addGUN, minGUN, addGUNAsync } from './index.redux';
+import { counter } from './index.redux';
+import { Provider } from 'react-redux';
 import App from './App1';
-const store = createStore(counter, applyMiddleware(thunk));
+
+const reduxDevtools = window.devToolsExtension ? window.devToolsExtension() : ()=>{};
+const store = createStore(counter, compose(
+    applyMiddleware(thunk),
+    reduxDevtools
+));
 
 // const store = createStore(counter);
 // const init = store.getState();
@@ -18,9 +25,15 @@ const store = createStore(counter, applyMiddleware(thunk));
 // store.dispatch({type: "addGun"});
 // store.dispatch({type: "minGun"});
 
-function render(){
-    ReactDOM.render(<App store={store} addGUN={addGUN} minGUN={minGUN} addGUNAsync={addGUNAsync} />, document.getElementById("root"));
-}
+// function render(){
+//     ReactDOM.render(<App store={store} addGUN={addGUN} minGUN={minGUN} addGUNAsync={addGUNAsync} />, document.getElementById("root"));
+// }
 
-render();
-store.subscribe(render);
+// render();
+// store.subscribe(render);
+ReactDOM.render(
+    (<Provider store={store} >
+        <App />,
+    </Provider>)
+    document.getElementById("root")
+);
