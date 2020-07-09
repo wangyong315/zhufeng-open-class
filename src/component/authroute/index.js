857f4c975af0d09c039bdf4a1bf789f585069127
wangyong315 <291140738@qq.com>
2020-07-09 23:30:27 +0800

    更新router用法

--- src/component/authroute/index.js
+++ src/component/authroute/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import Axios from 'axios';
+import { withRouter } from 'react-router-dom';
 
+const publicList = [ '/login', '/register'];
+
+@withRouter
 class AuthRoute extends React.Component{
     componentDidMount () {
+        const pathname = this.props.history.pathname
+        if (publicList.indexOf(pathname) > -1) {
+            return null
+        }
         // 获取用户信息
         Axios.get('/user/info')
             .then(res => {
                 if (res.status === 200) {
                     console.log('res.data', res.data);
+                    if (res.data.code === 0) {
+                        // 有登录信息的
+                    } else {
+                        this.props.history.push('/login')
+                    }
                 }
             })
     }
