59f51673b4eed1ef5b11274fd216fb4f558cae20
wangyong315 <291140738@qq.com>
2020-07-17 21:37:27 +0800

    注册交互实现

--- src/container/register/register.js
+++ src/container/register/register.js
@@ -24,26 +24,54 @@ class Register extends React.Component{
     register(){
         this.props.history.push('/register')
     }
+
+    handleChange(key, val){
+        this.setState({[key]: val})
+    }
+
+    handleRegister = () => {
+        console.log(this.state);
+    }
     render(){
         return (
             <div>
                 <Logo />
                 <WingBlank>
                     <List>
-                        <InputItem>用户</InputItem>
+                        <InputItem 
+                            onChange={v => this.handleChange('user', v)}
+                        >
+                            用户
+                        </InputItem>
                         <WhiteSpace />
-                        <InputItem>密码</InputItem>
+                        <InputItem
+                            type="password"
+                            onChange={v => this.handleChange('pwd', v)}
+                        >
+                            密码
+                        </InputItem>
                         <WhiteSpace />
-                        <InputItem>确认密码</InputItem>
-                        <RadioItem checked={this.state.type=="genius"}>
+                        <InputItem
+                            type="password"
+                            onChange={v => this.handleChange('repeatpwd', v)}
+                        >
+                            确认密码
+                        </InputItem>
+                        <RadioItem 
+                            checked={this.state.type=="genius"}
+                            onChange={v => this.handleChange('type', 'genius')}
+                        >
                             牛人
                         </RadioItem>
-                        <WhiteSpace />                        
-                        <RadioItem checked={this.state.type=="boss"}>
+                        <WhiteSpace />  
+                        <RadioItem 
+                            checked={this.state.type=="boss"}
+                            onChange={v => this.handleChange('type', 'boss')}
+                        >
                             老板
-                        </RadioItem>
+                        </RadioItem>                      
                         <WhiteSpace />                                                
-                        <Button type="primary">注册</Button>
+                        <Button type="primary" onClick={this.handleRegister}>注册</Button>
                     </List>
                     <WhiteSpace />    
                 </WingBlank>
