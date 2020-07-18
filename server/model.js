4d6f35ec170603cfa788db11ac3ea2cf91cab352
wangyong315 <291140738@qq.com>
2020-07-18 22:21:25 +0800

    注册功能实现

--- server/model.js
+++ server/model.js
@@ -5,3 +5,30 @@ mongoose.connect(DB_URL);
 mongoose.connection.on('connected', function(){
     console.log('mongo has connect sucessly');
 });
+
+
+const models = {
+    user: {
+        'user': {type: String, require: true},
+        'pwd': {type: String, require: true},
+        'type': {type: String, require: true},
+        'avatar': {type: String },
+        'desc': {type: String},
+        'title': {type: String},
+        'company': {type: String},
+        'money': {type: String},
+    },
+    chart: {
+
+    }
+}
+
+for (const m in models) {
+    mongoose.model(m, new mongoose.Schema(models[m]))
+}
+
+module.exports = {
+    getModel: function (name) {
+        return mongoose.model(name)
+    }
+}
