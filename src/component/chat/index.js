2e408147e25380fd92d729e9f712c958ed9f608e
wangyong315 <291140738@qq.com>
2020-08-06 21:24:24 +0800

    socket

--- src/component/chat/index.js
+++ src/component/chat/index.js
@@ -1,15 +1,41 @@
 import React from 'react';
+import { List, InputItem } from 'antd-mobile'
 import io from 'socket.io-client'
 
+const socket = io('ws://localhost:9093')
 class Chat extends React.Component{
 
+  state = {
+    text: ''
+  }
+
   componentDidMount(){
-    const socket = io('ws://localhost:9093')
+  }
+
+  handleSubmit = () => {
+    console.log('sgtasgasd');
+    socket.emit('sendmsg', function (params) {
+      
+    })
   }
 
   render (){
+    console.log('this.state', this.state);
     return (
-      <div>chat with user: {this.props.match.params.user}</div> 
+      <div className="stick-footer">
+        <List>
+          <InputItem
+            placeholder="请输入"
+            value={this.state.text}
+            onChange={ v => {
+              this.setState({text: v})
+            }}
+            extra={<span onClick={() => this.handleSubmit()}>发送</span>}
+          >
+            信息
+          </InputItem>
+        </List>
+      </div> 
     )
   }
 }
